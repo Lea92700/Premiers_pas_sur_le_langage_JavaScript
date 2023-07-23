e190887e883a36617103eccdce418a68fd91cc1d
Léa Germany <leagermany@yahoo.com>
2023-07-23 22:21:23 +0200

    sixieme commit

--- assets/script.js
+++ assets/script.js
@@ -59,6 +59,10 @@ const slides = [
 	  console.log(slides[clickCount]);
 	});
 
+	document.banner.innerHTML = '<img>'
+    + '<img class="'banner-img'" src="'/assets/images/slideshow/slide2.jpg'" />'
+    + '<p>'Tirages haute définition grand format' <span>'pour vos bureaux et events'</span>''</p>'
+    + '</img>'
 
 	updateCounter();
   
